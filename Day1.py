6dad0ce9c4eb5ef3d3e3c3d29f2c2ef8c2447caa
Daniel <demirovdaniil@gmail.com>
2026-02-18 22:31:01 +0100

    first commit

---
+++ Day1.py
@@ -0,0 +1,116 @@
+# TEXT
+# print('Hello World!',  end='')
+# print('That`s cool')
+
+# NUMBERS
+# print(3)
+# print(250)
+# print(3*5)
+# print("I`m Daniel", 18, "Love Python")
+# COMMENTS
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+#VARIABLES
+'''
+x = 5
+y = "John"
+print(x)
+print(y)
+'''
+'''
+x = 4   # x is of type int
+x = "Sally"   # x is now of type str
+print(x)
+'''
+'''
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+'''
+
+"""
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+"""
+
+#Присваивание Нескольких Значений
+"""
+x,y,z = "Banana", "Cherry", "Apple"
+print(x)
+print(y)
+print(z)
+
+x=y=z = "Orange"
+print(x)
+print(y)
+print(z)
+"""
+#Unpack a Collection
+"""
+fruits = ["apple", "banana", "cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+"""
+
+#Output Variables
+"""
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 28
+print(x+y)
+
+"""
+
+#Global Variables
+"""
+x = "awesome"
+def myfunc():
+    print("Python is" + x)
+
+myfunc()
+"""
+#///////
+"""
+x = " awesome"
+
+def myfunc():
+    x = " fantastic"
+    print("Python is" + x)
+
+myfunc()
+
+print("Python is" + x)
+///////
+def myfunc():
+    global x
+    x = " fantastic"
+
+myfunc()
+
+print("Python is" + x)
+"""
+
+x = " awesome"
+
+def myfunc():
+    global x
+    x = " fantastic"
+
+myfunc()
+print("Python is" + x)
+
