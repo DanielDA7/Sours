420f72b8d25bfe5adfd1b93c16a54a3c66305014
Daniel <demirovdaniil@gmail.com>
2026-02-19 20:22:55 +0100

    Python Output

--- PythonW3schools/Day1.py
+++ PythonW3schools/Day1.py
@@ -1,12 +1,4 @@
-# TEXT
-# print('Hello World!',  end='')
-# print('That`s cool')
 
-# NUMBERS
-# print(3)
-# print(250)
-# print(3*5)
-# print("I`m Daniel", 18, "Love Python")
 # COMMENTS
 """
 This is a comment
---
+++ PythonW3schools/Python Output.py
@@ -0,0 +1,9 @@
+#TEXT
+print('Hello World!',  end='')
+print('That`s cool')
+
+#NUMBERS
+print(3)
+print(250)
+print(3*5)
+print("I`m Daniel", 18, "Love Python")
